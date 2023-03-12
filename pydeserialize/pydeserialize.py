16e409c8cda654871dac64eba2b5d3feacf58c16
João <joaovictorti08@gmail.com>
2023-03-12 19:05:12 -0300

    Pydeserialize 0.0.4 version error

--- pydeserialize/pydeserialize.py
+++ pydeserialize/pydeserialize.py
@@ -16,7 +16,7 @@ def banner():
 def argumentos() -> None:
     
     parse = argparse.ArgumentParser(prog=banner(),usage="pyserialize -ip 192.168.4.113 -p 80 -o Windows",formatter_class=RawTextHelpFormatter)
-    parse.add_argument("--verison",action="version",version="pydeserialize 0.0.4")
+    parse.add_argument("--version",action="version",version="pydeserialize 0.0.4")
     parse.add_argument("-ip",action="store",type=str,dest="ip",required=True, help="Insert ip")
     parse.add_argument("-p",action="store",type=str,dest="port",required=True, help="Insert port")
     parse.add_argument("-e",action="store",type=str,dest="encode",choices=["b64","shell","urlencode","hex"],default="",required=True,help="Insert encoding")
